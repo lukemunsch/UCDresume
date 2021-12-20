0f816babbe221804af0cf14ae333c38f43f41009
lukemunsch <lpmunsch@gmail.com>
2021-12-20 22:14:30 +0000

    add content to js file

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -63,10 +65,15 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            } else {
+            } else if(errorResponse.status === 403){
+                var resetTime = new Date(errorResponse.getResponseHeader('x-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests - please wait until ${resetTime.toLocaleTimeString()}</h4>`)
+            }else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);

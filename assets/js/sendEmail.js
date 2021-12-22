77617cc1d2675496f7090c9d90dbeafb436021bd
lukemunsch <lpmunsch@gmail.com>
2021-12-22 20:41:45 +0000

    add content to js file to fix error

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -3,13 +3,13 @@ function sendMail(contactForm) {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "project_request": contactForm.projectsummary.value
-        })
-        .then(
-            function (response) {
-                console.log("SUCCESS!", response);
-            }
-
-            function (error) {
-                console.log("Failed...", error);
-            });
+    })
+    .then(
+        function (response) {
+            console.log("SUCCESS!", response);
+        },
+        function (error) {
+            console.log("Failed...", error);
+        }
+    );
 }
